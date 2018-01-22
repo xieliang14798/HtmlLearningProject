3a46e15fea1cb9b6bf024dbd6f6a858ffe4e90a3
谢良 <xieliang14798@hundsun.com>
2018-01-22 17:22:50 +0800

    11

--- page/JDMobile/js/index.js
+++ page/JDMobile/js/index.js
@@ -1,5 +1,34 @@
 'use strict';
 $(function () {
+    //导航栏颜色
+    var headerBox = $("#jd_header_box");
+    var jdBanner = $("#jd_banner");
+    var offsetHeight = jdBanner.height();
+    $(window).scroll(function () {
+        var top = $(this).scrollTop() / offsetHeight;
+        var opacity = top <= 1 ? top : 1;
+        headerBox.css('background', 'rgba(201, 21, 35, ' + 0.85 * opacity + ')');
+    });
+    //秒杀
+    var $skTime = $("#sk_time");
+    var timeList = $skTime.find(".num");
+    var time = 10 * 60 * 60;
+    var timer;
+    timer = setInterval(function () {
+        time--;
+        var hour = Math.floor(time / 3600);
+        var minute = Math.floor(time / 60 % 60);
+        var second = Math.floor(time % 60);
+        timeList[0].innerText = hour >= 10 ? Math.floor(hour / 10) : 0;
+        timeList[1].innerText = Math.floor(hour % 10);
+        timeList[2].innerText = minute >= 10 ? Math.floor(minute / 10) : 0;
+        timeList[3].innerText = Math.floor(minute % 10);
+        timeList[4].innerText = second >= 10 ? Math.floor(second / 10) : 0;
+        timeList[5].innerText = Math.floor(second % 10);
+        if (time <= 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
     //轮播图滑动事件
     var OFFSET = 50;
     $(".jd_banner").each(function (index, item) {
